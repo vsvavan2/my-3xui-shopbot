d096746a3029508ffa934a1ab70778362e51754f
vsvavan2 <vladosmakov2505@gmail.com>
2025-12-19 12:00:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ read_input_yn() {
     echo
 }
 
-REPO_URL="https://github.com/tweopi/3xui-shopbot.git"
-PROJECT_DIR="3xui-shopbot"
+REPO_URL="https://github.com/vsvavan2/my-3xui-shopbot.git"
+PROJECT_DIR="my-3xui-shopbot"
 NGINX_CONF_FILE="/etc/nginx/sites-available/${PROJECT_DIR}.conf"
 
 echo -e "${GREEN}--- Запуск скрипта установки/обновления 3xui-ShopBot ---${NC}"
